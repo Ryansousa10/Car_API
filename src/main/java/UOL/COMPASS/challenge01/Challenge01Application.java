93cd7b0f92dd4e88970e769478924d432ef96ea9
ryansousa10 <ryan.sousa4000@gmail.com>
2023-09-11 13:41:21 -0300

    first commit

---
+++ src/main/java/UOL/COMPASS/challenge01/Challenge01Application.java
@@ -0,0 +1,13 @@
+package UOL.COMPASS.challenge01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge01Application.class, args);
+	}
+
+}
---
+++ src/test/java/UOL/COMPASS/challenge01/Challenge01ApplicationTests.java
@@ -0,0 +1,13 @@
+package UOL.COMPASS.challenge01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
